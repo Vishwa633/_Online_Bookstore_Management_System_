b7e76a89bbf3bf8d84d01c4192b5b691863268a7
Thisul Herath <thisulh@gmail.com>
2025-05-18 23:07:47 +0530

    Update BookQuickSort.java

--- src/main/java/com/example/bookstore/bookmanagement/BookQuickSort.java
+++ src/main/java/com/example/bookstore/bookmanagement/BookQuickSort.java
@@ -28,4 +28,30 @@ public class BookQuickSort {
         books.set(high, temp);
         return i + 1;
     }
+
+    // Sort by price
+    public static void quickSortByPrice(List<Book> books, int low, int high) {
+        if (low < high) {
+            int pi = partitionByPrice(books, low, high);
+            quickSortByPrice(books, low, pi - 1);
+            quickSortByPrice(books, pi + 1, high);
+        }
+    }
+
+    private static int partitionByPrice(List<Book> books, int low, int high) {
+        double pivot = books.get(high).getPrice();
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (books.get(j).getPrice() < pivot) {
+                i++;
+                Book temp = books.get(i);
+                books.set(i, books.get(j));
+                books.set(j, temp);
+            }
+        }
+        Book temp = books.get(i + 1);
+        books.set(i + 1, books.get(high));
+        books.set(high, temp);
+        return i + 1;
+    }
 } 
